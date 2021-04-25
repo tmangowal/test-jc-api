66d5ef9e08ef91ce7c0fdd5e93b53e72de9b47a2
Theodore Mangowal <tmangowal@gmail.com>
2021-04-25 14:20:09 +0700

    Init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")();
+const cors = require("cors")
+
+express.use(cors());
+
+express.get("/", (req,res) => {
+  res.send("Express API")
+})
+
+express.listen(2021, () => {
+  console.log("Listening in port 2021")
+})
